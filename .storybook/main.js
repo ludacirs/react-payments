fb53143362945f9375ac77bc2d085bb64defc8cb
ludacirs <ludacirs@naver.com>
2022-01-20 16:12:02 +0900

    feat: 스토리북, 데코레이터/ 타입스크립트 설정 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,29 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  "stories": ['../src/components/', '../src/pages/'],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ],
-  webpackFinal: async config => {
-    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules', 'styles'];
+  stories: ["../src/components/", "../src/pages/"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app"],
+  webpackFinal: async (config) => {
+    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules", "styles"];
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@styles': path.resolve(__dirname, '../src/styles/'),
-      '@atoms': path.resolve(__dirname, '../src/components/atoms/'),
-      '@molecules': path.resolve(__dirname, '../src/components/molecules/')
+      "@styles": path.resolve(__dirname, "../src/styles/"),
+      "@atoms": path.resolve(__dirname, "../src/components/atoms/"),
+      "@molecules": path.resolve(__dirname, "../src/components/molecules/"),
     };
     return config;
   },
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import GlobalStyles from "../src/styles/global-styles";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +8,12 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  ),
+];
