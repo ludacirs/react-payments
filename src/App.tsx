c39a9e17932a42c4ffc54bb8d6b664096f2c9051
ludacirs <ludacirs@naver.com>
2022-01-20 16:12:15 +0900

    chore: 파일 추가/제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import Test from "@atoms/Test";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <Test/>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
